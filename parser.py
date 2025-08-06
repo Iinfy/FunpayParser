9fc01833753078f83dbde8d2cf0eb81ea2c0d7c0
Infy <user.mg1216@gmail.com>
2025-08-06 21:18:08 +0300

    fix(parsing): fixed error http code detection

--- parser.py
+++ parser.py
@@ -29,32 +29,36 @@ def offerParser(id,isGroupingOn):
     OffersList = []
     answer = requests.get(f"https://funpay.com/users/{id}/")
     log.info(f"Parsing user lots, Grouping: {isGroupingOn}, User ID: {id}, Status code: {answer.status_code}")
-    if not isGroupingOn:
-        html = BS(answer.content,'html.parser')
-        for offer in html.select(".mb20 > .offer"):
-            container = offer.select(".offer-tc-container")[0]
-            for item in container.select("a"):
-                desc = item.select_one(".tc-desc > .tc-desc-text").text
-                amount_elem = item.select_one(".tc-amount")
-                amount = amount_elem.text if amount_elem else "0"
-                price = item.select(".tc-price")[0].text
-                lot = Lot(desc,amount,price,id)
-                OffersList.append(lot)
+    if answer.status_code == 200:
+        if not isGroupingOn:
+            html = BS(answer.content,'html.parser')
+            for offer in html.select(".mb20 > .offer"):
+                container = offer.select(".offer-tc-container")[0]
+                for item in container.select("a"):
+                    desc = item.select_one(".tc-desc > .tc-desc-text").text
+                    amount_elem = item.select_one(".tc-amount")
+                    amount = amount_elem.text if amount_elem else "0"
+                    price = item.select(".tc-price")[0].text
+                    lot = Lot(desc,amount,price,id)
+                    OffersList.append(lot)
+        else:
+            html = BS(answer.content,'html.parser')
+            for offer in html.select(".mb20 > .offer"):
+                listName = offer.select_one(".offer-list-title-container").text
+                offerclass = OffersGroup(listName)
+                container = offer.select(".offer-tc-container")[0]
+                for item in container.select("a"):
+                    desc = item.select_one(".tc-desc > .tc-desc-text").text
+                    amount_elem = item.select_one(".tc-amount")
+                    amount = amount_elem.text if amount_elem else "0"
+                    price = item.select(".tc-price")[0].text
+                    lot = Lot(desc,amount,price,id)
+                    offerclass.offers.append(lot)
+                OffersList.append(offerclass)
+        return OffersList
     else:
-        html = BS(answer.content,'html.parser')
-        for offer in html.select(".mb20 > .offer"):
-            listName = offer.select_one(".offer-list-title-container").text
-            offerclass = OffersGroup(listName)
-            container = offer.select(".offer-tc-container")[0]
-            for item in container.select("a"):
-                desc = item.select_one(".tc-desc > .tc-desc-text").text
-                amount_elem = item.select_one(".tc-amount")
-                amount = amount_elem.text if amount_elem else "0"
-                price = item.select(".tc-price")[0].text
-                lot = Lot(desc,amount,price,id)
-                offerclass.offers.append(lot)
-            OffersList.append(offerclass)
-    return OffersList
+        log.error(f"Parsing error, Status code: {answer.status_code}")
+        return False
 
 @log.catch(level="ERROR")
 def review_parser(id):
