0922d30162d75750bbf5ad0a8dd632d421a0b0be
Infy <user.mg1216@gmail.com>
2025-08-07 13:28:49 +0300

    feat(ui): added warning for parsing frequency lower than 5s

--- ui.py
+++ ui.py
@@ -8,13 +8,18 @@ def start():
         mode = int(input("Choose mode(OfferParser) - Enter mode number: "))
         userid = input("Enter user id: ")
         parsing_frequency = float(input("Enter parsing frequency: "))
-        log.info(f"Settings received, Mode: {mode}, User ID: {userid}, Parsing frequency: {parsing_frequency}")
-        if mode == 1:
-            isGroupingOn = input("Group offers?(Yes/No): ").strip()
-            if isGroupingOn == "Yes":
-                pp.startParsing(userid,True,mode,parsing_frequency)
+        if parsing_frequency < 5:
+            print("Parsing frequency lower than 5 seconds may occur problems")
+            continue_with_current_speed = input("Continue? (Y/n): ")
+        if continue_with_current_speed.lower() == "y":
+            log.info(f"Settings received, Mode: {mode}, User ID: {userid}, Parsing frequency: {parsing_frequency}")
+            if mode == 1:
+                isGroupingOn = input("Group offers?(Yes/No): ").strip()
+                if isGroupingOn == "Yes":
+                    pp.startParsing(userid,True,mode,parsing_frequency)
+            else:
+                pp.startParsing(userid,False,mode,parsing_frequency)
         else:
-            pp.startParsing(userid,False,mode,parsing_frequency)
-                
+            continue
         
         
